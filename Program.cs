5303c72d93a1371544840e5d7dc6e27d94bbfdc5
Your Name <@DESKTOP-RV7D3QO>
2022-12-13 22:15:22 +0400

    Дописал методы работы с Order

--- Program.cs
+++ Program.cs
@@ -1,6 +1,44 @@
+using CloudShopApp.Model;
+using CloudShopApp.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// добавляем контекст БД в контейнер
+builder.Services.AddDbContext<CloudShopDbContext>();
+
+// сервисы
+builder.Services.AddTransient<IOrderServicecs, DbOrderService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// ЗАДАНИЕ: 
+// Реализовать CRUD-операции для созданной сущности
+// Сделать DAO-интерфейс сервиса, и его имплементацию
+// Использовать инъекцию зависимостей*
+// Сделать простые контроллеры и протестировать методы CRUD-а
+// + зафиксировать изменения в ветке dev и запушить на сервер
+
+
+// пинги
+app.MapGet("/", () => "pong");
+app.MapGet("/ping", () => "pong");
+
+
+// простые контроллеры для тестирования CRUD-операций
+app.MapGet("/all", async (HttpContext context, IOrderServicecs service) =>
+{
+    await context.Response.WriteAsJsonAsync(service.GetAllOrders());
+});
+
+app.MapGet("/get-id", async (HttpContext context, IOrderServicecs service) =>
+{
+    int id = Convert.ToInt32(context.Request.Query["id"]);
+    await context.Response.WriteAsJsonAsync(service.GetOrderById(id));
+});
+
+// 
+
+// ПРОЧИЕ CRUD-операции
 
 app.Run();
